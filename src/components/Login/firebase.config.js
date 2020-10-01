2db0e9d23f776d0a96c2fcc82065d4eed998e1f8
JRakin <juaidrakin1@gmail.com>
2020-10-02 02:27:12 +0600

    initial commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBvZDMngi3RTmAE2zc-p6fcm5USXtHYWkY',
+  authDomain: 'burj-al-arab-f16.firebaseapp.com',
+  databaseURL: 'https://burj-al-arab-f16.firebaseio.com',
+  projectId: 'burj-al-arab-f16',
+  storageBucket: 'burj-al-arab-f16.appspot.com',
+  messagingSenderId: '222461216906',
+  appId: '1:222461216906:web:eb0622af5230d6bd0a8573',
+};
+
+export default firebaseConfig;
